75234f1f848b5f9563d18a7263ed60e5c8160283
vladraizer02 <34063010+vladraizer02@users.noreply.github.com>
2022-03-24 09:19:34 +0300

    Update and rename test.s to task4.s

--- task 4/task4.s
+++ task 4/task4.s
@@ -2,28 +2,34 @@ SECTION .text
 global getCpuName
 getCpuName:
 
-mov    eax,80000002h
-mov    rsi, buffer 
-cpuid
-mov    [rsi   ],eax
-mov    [rsi+ 4],ebx
-mov    [rsi+ 8],ecx
-mov    [rsi+12],edx
+push rbp
+mov rbp, rsp
+mov rdi, buffer
 
-mov    eax,80000003h
+mov    eax,80000002
+cpuid
+mov    [rdi   ],eax
+mov    [rdi+ 4],ebx
+mov    [rdi+ 8],ecx
+mov    [rdi+12],edx
+
+mov    eax,80000003
 cpuid
 mov    [rsi+16],eax
 mov    [rsi+20],ebx
 mov    [rsi+24],ecx
 mov    [rsi+28],edx
 
-mov    eax,80000004h
+mov    eax,80000004
 cpuid
 mov    [rsi+32],eax
 mov    [rsi+36],ebx
 mov    [rsi+40],ecx
 mov    [rsi+44],edx
-ret 
+
+mov eax, buffer
+pop rbp
+ret
 
 SECTION .bss
 buffer: resb 48 
